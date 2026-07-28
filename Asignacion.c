8a75b9eea76bdfb5ba128d82f306cc5b5d2d7643
RECHDIP <theglobalsc@gmail.com>
2026-07-28 16:23:45 -0400

    first commit

---
+++ Asignacion.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <dirent.h>
+
+
+void ejecutar_proceso();
+void listado_procesos();
+void Kill_bill();
+void ejecutar_en_segundo_plano(const char *comando);
+
+
+int main()
+{
+
+    int opcion;
+
+    do
+    {
+        system("clear");
+        printf("Menu \n\n");
+        printf("1.- Ejecutar un nuevo proceso \n");
+        printf("2.-Listado de procesos \n");
+        printf("3.-Matar un proceso \n");
+        printf("4.-Salir \n\n");
+        printf(" Que desea escoger?: ");
+        scanf("%d", &opcion);
+        printf("\n");
+
+        switch (opcion)
+        {
+
+        case 1:
+            printf("Hola 1 \n");
+            ejecutar_proceso();
+            break;
+        case 2:
+            printf("Hola 2 \n");
+            listado_procesos();
+            break;
+        case 3:
+            printf("Hola 3 \n");
+            Kill_bill();
+            break;
+        case 4:
+            printf("Saliendo del programa.. \n");
+
+            break;
+        default:
+            printf("Hola default \n");
+            break;
+        }
+    } while (opcion != 4);
+    printf("\n");
+    return 0;
+}
